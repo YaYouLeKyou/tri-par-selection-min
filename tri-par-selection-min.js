cd2e5fbcb854ba966ea9cf93ed7f0fa79e6c6010
Yayou <78508960+YaYouLeKyou@users.noreply.github.com>
2021-07-26 00:31:06 +0200

    Add files via upload

---
+++ tri-par-selection-min.js
@@ -0,0 +1,20 @@
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+
+    [array[i], array[min]] = [array[min], array[i]];
+  }
+  // Only change code below this line
+  return array;
+  // Only change code above this line
+}
+console.log(
+  selectionSort([
+    1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92,
+  ])
+);
